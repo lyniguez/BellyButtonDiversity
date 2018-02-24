beb39f9f9c93c497f03909bd2656a19de30ed4ac
lyniguez@gmail.com <lyniguez@Gmail.com>
2018-02-24 14:41:36 -0800

    updates

--- static/js/app.js
+++ static/js/app.js
@@ -1,85 +1,87 @@
 /* data route */
-var url = "/samples/"+samplenameselect;
-var samplenameselect = document.getElementById("nameSampleSelect");
+// var url = "/samples/"+samplenameselect;
+// var samplenameselect = document.getElementById("nameSampleSelect");
 
-function buildPlot(chosenSample) {
-    Plotly.d3.json(url, function(error, response) {
-        console.log(response);
+function selectedOption(value){
+    console.log(value);  
+};
+
+selectedOption();
+
+// function buildPlot(chosenSample) {
+//     Plotly.d3.json(`/metadata/${chosenSample}`, function(error, response) {
+//         console.log(response);
             
 
-        var data = [{
-            values: response.map(data => data.sample_values),
-            labels: response.map(data => data.otu_ids),
-            type: "pie"
-        }];
+//         var data = [{
+//             values: response.map(data => data.sample_values),
+//             labels: response.map(data => data.otu_ids),
+//             type: "pie"
+//         }];
 
-        var layout = {
-            title: "Top 10 samples by otu_id",
-            height: 600,
-            width: 800
-        };
+//         var layout = {
+//             title: "Top 10 samples by otu_id",
+//             height: 600,
+//             width: 800
+//         };
 
-        Plotly.newPlot("pie", data, layout);
-    }
+//     Plotly.newPlot("pie", data, layout)});
+    
+//     function updatePlotly(newdata) {
+//         var PIE = document.getElementById("pie");
+//         Plotly.restyle(PIE, "values", [newdata]);
+//     }
 
+//     function optionChanged(namesample) {
+//         var nameSampleSelect = document.getElementById("selDataset");
+//     }
+    
+// updatePlotly(namesample);
 
-
-    function updatePlotly(newdata) {
-        var PIE = document.getElementById("pie");
-        Plotly.restyle(PIE, "values", [newdata]);
-    }
-
-    function optionChanged(namesample) {
-        var nameSampleSelect = document.getElementById("selDataset");
-    }
-    updatePlotly(namesample);
-
-});
-
-buildPlot();
+// buildPlot();
 
 
 
-function buildPlot(chosenSample) {
-    Plotly.d3.json(url, function(error, response) {
-        console.log(response);
+// function buildPlot(chosenSample) {
+//     Plotly.d3.json(url, function(error, response) {
+//         console.log(response);
             
 
-        // bubble chart
-var trace1 = {
-    x: response.map(data => data.otu_ids),
-    y: response.map(data => data.sample_values),
-    mode: 'markers',
-    marker: {
-      size: response.map(data => data.sample_values),
-    }
-  };
+//         // bubble chart
+// var trace1 = {
+//     x: response.map(data => data.otu_ids),
+//     y: response.map(data => data.sample_values),
+//     mode: 'markers',
+//     marker: {
+//       size: response.map(data => data.sample_values),
+//     }
+//   };
   
-  var data = [trace1];
+//   var data = [trace1];
   
-  var layout = {
-    title: 'Marker Size',
-    showlegend: false,
-    height: 600,
-    width: 600
-  };
+//   var layout = {
+//     title: 'Marker Size',
+//     showlegend: false,
+//     height: 600,
+//     width: 600
+//   };
   
-  Plotly.newPlot('myDiv', data, layout);
-}
+//   Plotly.newPlot('myDiv', data, layout);
+// }
 
 
 
-    function updatePlotly(newdata) {
-        var PIE = document.getElementById("pie");
-        Plotly.restyle(PIE, "values", [newdata]);
-    }
+//     function updatePlotly(newdata) {
+//         var PIE = document.getElementById("pie");
+//         Plotly.restyle(PIE, "values", [newdata]);
+//     }
 
-    function optionChanged(namesample) {
-        var nameSampleSelect = document.getElementById("selDataset");
-    }
-    updatePlotly(namesample);
+//     function optionChanged(namesample) {
+//         var nameSampleSelect = document.getElementById("selDataset");
+//     }
+//     updatePlotly(namesample);
 
-});
+// });
 
-buildPlot();
+// buildPlot();
     
